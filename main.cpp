8be9b68b772f79c886b98a0e8b50b624bb9807a1
1adrian11 <163025137+1adrian11@users.noreply.github.com>
2024-09-18 22:03:57 +0200

    javitott kod

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //hibas valtozonev
+    int *b = new int[N_ELEMENTS]; //hibas valtozonev
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;) //hianyos ciklus
+    for (int i = 0; i<100; i++) //hianyos ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //max ertek hiany
+    for (int i = 0; i<100; i++) //max ertek hiany
     {
-        std::cout << "Ertek:" //adott elem megadasa
+        std::cout << "Ertek:" << b[i] << std::endl; //adott elem megadasa, ";" hiany
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i] //";" hiany
+        atlag += b[i]; //";" hiany
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
