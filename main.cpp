aa5f620e37e17d29a62b1770562eaa4c53d8d190
1adrian11 <163025137+1adrian11@users.noreply.github.com>
2024-09-18 21:59:41 +0200

    hibakomment

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS]; //hibas valtozonev
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    for (int i = 0;) //hianyos ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //max ertek hiany
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //adott elem megadasa
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //";" hiany
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
